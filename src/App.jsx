a3521135b9dee414acbe4cd241db739fc99cbcdb
MENDU-SREEKANTH-REDDY <143696588+MENDU-SREEKANTH-REDDY@users.noreply.github.com>
2025-07-06 23:36:51 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Itstaffing from "./components/Itstaffing";
+import Rpo from "./components/Rpo";
+import Bpo from "./components/Bpo"; 
+import Team from "./components/Team";
+import PayrollManagement from "./components/PayrollManagement";
+// import Contact from "./components/Contact";
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/staffing" element={<Itstaffing />} />
+        <Route path="/RPO" element={<Rpo />} />
+        <Route path="/BPO" element={<Bpo />} /> 
+        <Route path="/team" element={<Team />} />
+        <Route path="/payroll" element={<PayrollManagement />} />
+        {/* <Route path="/contact" element={<Contact/>}/> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
